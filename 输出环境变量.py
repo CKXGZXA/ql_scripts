df6f16f559b8b8de6849a06f67e314cbb1441b0e
赵希奥 <ckxgzxa@gmail.com>
2024-09-29 01:39:32 +0800

    feat: output JD_COOKIE to log

--- 输出环境变量.py
+++ 输出环境变量.py
@@ -1,10 +1,10 @@
 """
 name:输出环境变量
-cron:0 0 0 * * *
+cron:0 0 3 * * *
 """
 
 import os
 
 env = os.environ
-for key in env:
-    print(key, env[key])
+# 将JD_COOKIE的值输出到日志
+print(env.get('JD_COOKIE'))
